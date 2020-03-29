04f8ef642b297dd77620c295db2b0f32c6112387
WaterZhang97 <31640986+WaterZhang97@users.noreply.github.com>
2020-03-30 00:01:15 +0800

    feature: 1.增加多线程处理 2.增加写入hosts 3.自动清除原有github hosts

--- getip.py
+++ getip.py
@@ -1,15 +1,27 @@
 # 获取github和其子域名的IP地址
+import os
 import re
 import platform
+from shutil import copyfile, copy
+
 import requests
 from bs4 import BeautifulSoup
 import threading
+
+from requests.adapters import HTTPAdapter
+
+hosts = []
+thread_pool = []
+start_line = "# GitHub host start"
+end_line = "# GitHub host end"
 
 
 def get_host():
     for i in open("url.txt"):
         url = i.strip()
         t = MyThread(url)
+        t.setName(url)
+        thread_pool.append(t)
         t.start()
 
 
@@ -21,53 +33,93 @@ class MyThread(threading.Thread):
 
     def run(self):
         host = self.url
-        self.url = "http://ip.chinaz.com/" + self.url
-        resp = requests.get(self.url)
-        soup = BeautifulSoup(resp.text, features="html.parser")
-        x = soup.find(class_="IcpMain02")
-        x = x.find_all("span", class_="Whwtdhalf")
-        print(x[5].string.strip(), host)
+        self.url = "http://ip.chinaz.com/" + host
+        try:
+            http_session = requests.Session()
+            http_session.mount('http://', HTTPAdapter(max_retries=3))
+            request = http_session.get(self.url, timeout=60)
+            soup = BeautifulSoup(request.text, features="html.parser")
+            ips = soup.find(class_="IcpMain02")
+            ips = ips.find_all("span", class_="Whwtdhalf")
+            group_num = (len(ips) - 4) / 4
+            for i in range(int(group_num)):
+                hosts.append(ips[(i + 1) * 4 + 1].string.strip() + " " + host)
+                print(ips[(i + 1) * 4 + 1].string.strip() + " " + host)
+        except Exception as e:
+            print(e)
 
 
 host_window = r"C:\Windows\System32\drivers\etc\hosts"
 host_mac = r"/etc/hosts"
-UAT_HOST = ["#B2B uat deepblue2",
-            '10.128.xx.xx uat.xx.com',
-            '10.128.xx.xx xx.com',
-            '10.128.xx.xx xx.com']
-
-HOST_LIVE = ["#LIVE",
-             "104.20.xx.xx       xxx.com",
-             "104.20.xx.xx       www.xxx.com",
-             "104.20.xx.xx       www.xxx.br"]
 
 
-def search_host(hostvalue, host_path):
-    hostfile = open(host_path, 'r')
-    each_line = hostfile.readlines()
-    hostfile.close()
-    findresult = re.findall(hostvalue, ''.join(each_line))
-    return findresult
+def wait_thread_result():
+    stop_flag = True
+    while stop_flag:
+        for thread in thread_pool:
+            if thread.isAlive():
+                stop_flag = True
+                break
+            stop_flag = False
 
 
-def write_host(host_value):
+def clean_host():
+    with open(host_window, 'r', encoding='UTF-8') as host_file:
+        contents = host_file.readlines()
+        start_line_num = 0
+        end_line_num = 0
+        for i, content in enumerate(contents):
+            if content.find(start_line) != -1:
+                start_line_num = i
+            if content.find(end_line) != -1:
+                end_line_num = i
+        if start_line_num != 0 and end_line_num != 0:
+            current_line_num = start_line_num
+            while current_line_num <= end_line_num:
+                del contents[start_line_num]
+                current_line_num += 1
+    with open(host_window, 'w+', encoding='UTF-8') as host_file:
+        for content in contents:
+            host_file.write(content)
+
+
+def delete_line(old_file, new_file, del_line):
+    current_line = 0
+    while current_line < (del_line - 1):
+        old_file.readline()
+        current_line += 1
+    # 当前光标在被删除行的行首，记录该位置
+    seek_point = old_file.tell()
+    # 设置光标位置
+    new_file.seek(seek_point, 0)
+    # 读需要删除的行，光标移到下一行行首
+    old_file.readline()
+    # 被删除行的下一行读给 next_line
+    next_line = old_file.readline()
+    # 连续覆盖剩余行，后面所有行上移一行
+    while next_line:
+        new_file.write(next_line)
+        next_line = old_file.readline()
+    # 写完最后一行后截断文件，因为删除操作，文件整体少了一行，原文件最后一行需要去掉
+    new_file.truncate()
+
+
+def write_host(hosts_value):
     if platform.system() is "Windows":
         output = open(host_window, 'a')
     else:
         output = open(host_mac, 'a')
 
-    output.write("# GitHub Start")
-    for inside in host_value:
-        output.write(inside)
-        output.newlines()
-    output.write("# GitHub End")
+    output.write("\n")
+    output.write(start_line + "\n")
+    for inside in hosts_value:
+        output.write(inside + "\n")
+    output.write(end_line)
     output.close()
 
 
 if __name__ == "__main__":
     get_host()
-
-    # if search_host(HOST_LIVE[0], host_window):
-    #     print("it exist, no need to update")
-    # else:
-    #     write_host(HOST_LIVE)
+    wait_thread_result()
+    clean_host()
+    write_host(hosts)
